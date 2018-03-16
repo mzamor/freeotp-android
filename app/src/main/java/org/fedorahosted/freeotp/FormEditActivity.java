b6ea8c7f17f4db11af6453b6aaf95615b06b5fd3
mariano <madazamo@gmail.com>
2018-03-16 07:53:56 -0300

    Falta mejorar pantalla formulario manual, tags de los botones y listview. Arreglé la estetica del boton del formulario manual y lo linkie al Activity2Main.

--- app/src/main/java/org/fedorahosted/freeotp/FormEditActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/FormEditActivity.java
@@ -2,18 +2,21 @@ package org.fedorahosted.freeotp;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import org.fedorahosted.freeotp.edit.BaseActivity;
 
 public class FormEditActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Button cargar_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,17 @@ public class FormEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_form_edit);
 
         toolbar = (Toolbar) findViewById(R.id.form_edit_toolbar);
+        cargar_btn=(Button) findViewById(R.id.button_cargar);
+
+        cargar_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FormEditActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Cargar manualmente");
